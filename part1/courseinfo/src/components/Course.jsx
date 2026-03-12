7f70d20b885ca6e4492eb8e43971bd855f60e990
Raymond Rached <raymondorached@gmail.com>
2026-03-12 16:52:43 +0200

    courseinfo steps 6->10

---
+++ part1/courseinfo/src/components/Course.jsx
@@ -0,0 +1,33 @@
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts.reduce((sum,part) => sum+part.exercises,0)
+        }
+      />
+    </div>
+  )
+}
+
+const Header = ({course}) => <h1>{course}</h1>
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Part = ({part}) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({total}) => <p><b>total of {total} exercises</b></p>
+
+export default Course

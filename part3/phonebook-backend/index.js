5e0a1042ee9f5983d690a91ebc4c0e69b0f45e6e
Raymond Rached <raymondorached@gmail.com>
2026-04-03 17:01:16 +0300

    completed phonebook backend steps 7 & 8 (finished 3a)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json());
+morgan.token('POST_content', function (req, res) {
+    if (req.method === 'POST'){
+        return JSON.stringify(req.body)
+    }
+    return null
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :POST_content'))
 
 persons = [
     { 

07337d0ff5288f63592dfb8cc542ca150b676113
Raymond Rached <raymondorached@gmail.com>
2026-04-15 16:21:52 +0300

    Phonebook database step 1&2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Person = require('./models/person')
 const morgan = require('morgan')
 const app = express()
 
@@ -13,31 +15,10 @@ morgan.token('POST_content', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :POST_content'))
 
-persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons',(request,response) => {
-    response.json(persons)
+    Person.find({}).then(persons => 
+        response.json(persons)
+    )
 })
 
 app.get('/api/persons/:id',(request,response) => {
@@ -75,17 +56,16 @@ app.post('/api/persons',(request,response) => {
             error:"number is missing"
         })  
     }
-    if (persons.find(p => p.name === body.name)){
-        return response.status(409).json({
-            error:"name must be unique"
-        })  
-    }
-    const person = {...body,id:String(Math.floor(Math.random()*10000000))}
-    persons.push(person)
-    response.json(person)
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number
+    })
+    newPerson.save().then((savedPerson) =>{
+        response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT,() => {
     console.log(`Server running on port ${PORT}`)
 })

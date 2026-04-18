e5b059b60cc9d7725ad34c2d221fc09c3634e8ac
Raymond Rached <raymondorached@gmail.com>
2026-04-18 21:48:10 +0300

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -15,13 +15,13 @@ morgan.token('POST_content', function (req, res) {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :POST_content'))
 
-app.get('/api/persons',(request,response) => {
+app.get('/api/persons',(request,response,next) => {
     Person.find({}).then(persons => 
         response.json(persons)
     ).catch(error => next(error))
 })
 
-app.get('/api/persons/:id',(request,response) => {
+app.get('/api/persons/:id',(request,response,next) => {
     Person.findById(request.params.id)
     .then((person) => {
         if (!person){
@@ -31,7 +31,7 @@ app.get('/api/persons/:id',(request,response) => {
     }).catch(error => next(error))
 })
 
-app.get('/info',(request,response) => {
+app.get('/info',(request,response,next) => {
     console.log('hi')
     Person.countDocuments({}).then((length) => {
         const date = new Date()
@@ -40,14 +40,14 @@ app.get('/info',(request,response) => {
     }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request,response) => {
+app.delete('/api/persons/:id',(request,response,next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result =>
         response.status(204).end()
     ).catch(error => next(error))
 })
 
-app.post('/api/persons',(request,response) => {
+app.post('/api/persons',(request,response,next) => {
     const body = request.body
     if (!body.name){
         return response.status(400).json({
@@ -68,17 +68,16 @@ app.post('/api/persons',(request,response) => {
     }).catch(error => next(error))
 })
 
-app.put('/api/persons/:id',(request,response) => {
-    Person.findById(request.params.id)
-    .then((person) => {
-        if (!person){
-            return response.status(404).end()
-        }
-        person.number = request.body.number
-        return person.save().then(updatedPerson => 
-            response.json(updatedPerson)
-        )
-    }).catch(error => next(error))
+const opts = { returnDocument: 'after', runValidators: true, context: 'query' };
+
+app.put('/api/persons/:id',(request,response,next) => {
+    console.log(request.params.id,{...request.body})
+    Person.findOneAndUpdate({_id:request.params.id},{...request.body},opts)
+    .then(updatedPerson => {
+        if (!updatedPerson) return response.status(404).end()
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -86,6 +85,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error === 'CastError'){
         return response.status(400).send({error:"malformed id"})
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message , name: error.name})
     }
 
     next(error)
